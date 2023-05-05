2cc4ad6a34a7e5d5a547e5034c38d006c8aab003
maikyramon <maiky.ramon@gmail.com>
2023-05-04 22:55:54 -0300

    ajustada calculadora pra distance e melee

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from tkinter import *
 from tkinter import messagebox
 from math import *
 
-#skill_constant = {'ml': 1600, 'melee': 50, 'distance': 25, 'shielding': 100, 'fishing': 20}
 vocation_constants = {
     '1': {'1600' : 3.0, '50': 1.1, '51': 1.1, '25': 1.4, '100': 1.1, '20': 1.1},
     '2': {'1600' : 1.4, '50': 1.2, '51': 1.2, '25': 1.1, '100': 1.1, '20': 1.1},
@@ -11,11 +10,9 @@ vocation_constants = {
     '5': {'1600' : 4.0, '50': 2.0, '51': 1.5, '25': 2.0, '100': 1.5, '20': 1.1}
 }
 
-
 janela = Tk()
 
 janela.title("calc skill tibia")
-
 
 val_vocation = StringVar()
 val_skill    = StringVar()
@@ -38,27 +35,16 @@ skills = [("ml",        1600, 1),
 
 
 def calc_skill():
-#b = vocation constant
-#c = skill offset  (0 for ml, 10 for other skills)
-#Tp = total points
-#P = points
-
-# P = A.b^skill-c
-
-#Total points at certain skill level
-#TP = A . ((b^skill-c - 1)/b-1)
-
-# skill at certain level
-# skill = log(b).((b-1)/A + 1) + c
     global atual
     global desejada
 
     A = val_skill.get()
     b = val_vocation.get()
-    skill = int(atual.get())
+    skill_atual = int(atual.get())
+    skill_desejada = int(desejada.get())
     b = float(vocation_constants[b][A])
-    A = int(A)
 
+    A = int(A)
     if A == 51: 
         A -= 1
 
@@ -67,12 +53,19 @@ def calc_skill():
     else: 
         c = 10
     
-    P = A * pow(b, skill - c)
+    P = A * ((pow(b, skill_atual - c) - 1) / (b - 1))
+    TP = A * ((pow(b, skill_desejada - c) - 1) / (b - 1))
+    
+    TP = TP - P
 
-    #b = int(b)
+    sec = int(TP)
 
-    messagebox.showinfo("Information",f'b:{b}, A:{A}, skill: {skill}, c: {c}, P: {P}')
-    #return 0
+    hour = int(sec / 3600)
+    sec  = sec % 3600
+    min  = int(sec / 60)
+    sec  = sec % 60
+    
+    messagebox.showinfo("Information",f'{hour} hours, {min} minutes, {sec} seconds, ou {int(TP)} golpes')
 
 
 Label(janela, text="vocação: ").grid(column=0, row=0)
@@ -98,31 +91,3 @@ Button(janela, text="calc", width=10, command=calc_skill).grid(column=1, row=4)
 
 
 janela.mainloop()
-
-
-
-
-
-
-
-#A = Skill constant  
-# A {
-    # ml = 1600
-    # melee = 50
-    # distance = 25
-    # shielding = 100
-    # fishing = 20
-# }
-
-#b = vocation constant
-
-
-
-# skill_constant = {'ml': 1600, 'melee': 50, 'distance': 25, 'shielding': 100, 'fishing': 20}
-# no = {'magic' : 4.0, 'melee': 2.0, 'fist': 1.5, 'distance': 2.0, 'distance': 1.5, 'fishing': 1.1}
-# ek = {'magic' : 3.0, 'melee': 1.1, 'fist': 1.1, 'distance': 1.4, 'distance': 1.1, 'fishing': 1.1}
-# rp = {'magic' : 1.4, 'melee': 1.2, 'fist': 1.2, 'distance': 1.1, 'distance': 1.1, 'fishing': 1.1}
-# ms = {'magic' : 1.1, 'melee': 2.0, 'fist': 1.5, 'distance': 2.0, 'distance': 1.5, 'fishing': 1.1}
-# ed = {'magic' : 1.1, 'melee': 1.8, 'fist': 1.5, 'distance': 1.8, 'distance': 1.5, 'fishing': 1.1}
-
-
